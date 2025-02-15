f39dbc6e3fc62d82ed61febdc5326fb3ee087a7e
pete3n <pete3n@protonmail.com>
2025-02-15 13:57:39 -0500

    feat: check Neovim version, check plugins, refactor

--- lua/ninjection/health.lua
+++ lua/ninjection/health.lua
@@ -1,17 +1,44 @@
 local health = require("vim.health")
+local start = health.start or health.report_start -- Nvim 0.11 deprication
+local ok = health.ok or health.report_ok
+local warn = health.warn or health.report_warn
+local error = health.warn or health.report_error
 
 local M = {}
 
-function M.check()
-	health.start("ninjection.nvim Health Check")
+local required_plugins = {
+  { lib = "nvim-lspconfig", optional = false,
+		info = "Required for LSP integration" },
+  { lib = "nvim-treesitter",
+    optional = false,
+    info = "Required for injected language parsing",
+  },
+}
 
-  -- Check for the Treesitter dependency (nvim-treesitter)
-  local ok, _ = pcall(require, "nvim-treesitter")
-  if ok then
-    health.ok("nvim-treesitter is installed.")
-  else
-    health.error("nvim-treesitter is not installed. Please install it.")
+local function lualib_installed(lib_name)
+  local res, _ = pcall(require, lib_name)
+  return res
+end
+
+function M.check()
+	start "Checking Neovim version >= 0.8"
+	if vim.version().major == 0 and vim.version().minor < 8 then
+		error("Neovim 0.8 or greater required")
 	end
+
+	start "Checking for required plugins"
+	for _, plugin in ipairs(required_plugins) do
+    if lualib_installed(plugin.lib) then
+      ok(plugin.lib .. " installed.")
+    else
+      local lib_not_installed = plugin.lib .. " not found."
+      if plugin.optional then
+        warn(("%s %s"):format(lib_not_installed, plugin.info))
+      else
+        error(lib_not_installed)
+      end
+    end
+  end
 end
 
 return M
