553e6244497f6ca62eaae3a64e63ebf15296617e
pete3n <pete3n@protonmail.com>
2025-02-15 16:52:08 -0500

    feat: select command

--- plugin/ninjection.lua
+++ plugin/ninjection.lua
@@ -1,2 +1,4 @@
 vim.api.nvim_create_user_command("NJedit", require("ninjection").create_child_buffer, {})
 vim.api.nvim_create_user_command("NJSyncChild", require("ninjection").sync_child, {})
+vim.api.nvim_create_user_command("NJSelect", require("ninjection").select, {})
+
