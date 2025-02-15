e6bec0939253ac5955d9dd0734b96c8458b53f41
pete3n <pete3n@protonmail.com>
2025-02-15 11:54:12 -0500

    fix: file naming

--- lua/ninjection/util.lua
+++ lua/ninjection/util.lua
@@ -10,26 +10,28 @@ end
 -- filetypes to buffers, so we need our on function to start the appropriate
 -- LSP.
 M.start_lsp = function(lang, root_dir)
-	local lang_lsp = nil
-
-	-- There must be an LSP associated with the injected language
-	for k, v in pairs(cfg.lsp_map) do
-		if k == lang then
-			lang_lsp = v
-			break
-		end
-	end
+	local lang_lsp = cfg.lsp_map[lang]
 
   if not lang_lsp then
 		vim.notify(
-			"ninjeciton WARNING: No LSP configured for language: " .. lang .. " check your configuration.",
+			"ninjection WARNING: No LSP mapped to language: " .. lang .. " check your configuration.",
 			vim.log.levels.WARN
 		)
-    return {"unavailable", -1}
+    return {"unmapped", -1}
   end
 
+	-- The LSP must have an available configuration
+	local lsp_def = lspconfig[lang_lsp]
+	if not lsp_def.config_def or not lsp_def.config_def.default_config then
+		vim.api.nvim_err_writeln(
+			"ninjection ERROR: Could not find configuration for " .. lang_lsp ..
+			". Ensure it is installed and properly configured for lspconfig."
+		)
+		return {"unconfigured", -1}
+	end
+
 	-- The LSP must be available to execute
-	local lsp_cmd = lspconfig[lang_lsp].config_def.default_config.cmd
+	local lsp_cmd = lsp_def.config_def.default_config.cmd
 	if not lsp_cmd then
 		vim.api.nvim_err_writeln(
 			"ninjection ERROR: Command found to execute " .. lang_lsp ..
@@ -39,14 +41,7 @@ M.start_lsp = function(lang, root_dir)
 	end
 
 	-- The LSP must support our injected language
-	local lang_supported = false
-	for _, v in ipairs(lspconfig[lang_lsp].config_def.default_config.filetypes) do
-		if v == lang then
-			lang_supported = true
-		end
-	end
-
-	if not lang_supported then
+	if not vim.tbl_contains(lsp_def.config_def.default_config.filetypes, lang) then
 		vim.api.nvim_err_writeln(
 			"ninjection ERROR: " .. lang_lsp .. " does not support " ..
 			lang .. " modify your configuration to an appropriate LSP."
@@ -66,7 +61,7 @@ M.start_lsp = function(lang, root_dir)
 			" did not start correctly, check your language client log (default ~/.local/state/nvim/lsp.log) " ..
 			"for more information."
 		)
-		return {"start_error", -1}
+		return {"failed_start", -1}
 	else
 		return {"started", client_id}
 	end
