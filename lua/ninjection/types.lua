c4117f0b68e581a8a2927139141fe90287cced5f
pete3n <pete3n@protonmail.com>
2025-02-23 09:17:37 -0500

    Revert "chore: meta annotation, lspconfig.Config type def"
    This reverts commit 590444918ae5b3861118602da2347c09c1458a43.

--- lua/ninjection/types.lua
+++ lua/ninjection/types.lua
@@ -1,10 +1,3 @@
----@meta
-
----@alias EditorStyle "cur_win" | "floating" | "v_split" | "h_split"
--- Modified from nvim-lspconfig/lua/lspconfig/configs.lua because I can't find
--- a reference to: vim.api.keyset.create_user_command.command_args
----@alias lspconfig.Config.command {[1]:string|vim.api.keyset.user_command}
-
 ---@class NJRange
 ---@field s_row integer
 ---@field s_col integer
@@ -35,15 +28,4 @@
 --- "unconfigured", "unavailable", "no-exec", "unsupported", "failed_start", "started"
 ---@field client_id integer -- The client ID of the started LSP, or -1 on failure
 
--- Helper annotation for lspconfig from nvim-lspconfig/lua/lspconfig/configs.lua
---- @class lspconfig.Config : vim.lsp.ClientConfig
---- @field enabled? boolean
---- @field single_file_support? boolean
---- @field silent? boolean
---- @field filetypes? string[]
---- @field filetype? string
---- @field on_new_config? fun(new_config: lspconfig.Config?, new_root_dir: string)
---- @field autostart? boolean
---- @field package _on_attach? fun(client: vim.lsp.Client, bufnr: integer)
---- @field root_dir? string|fun(filename: string, bufnr: number)
---- @field commands? table<string, lspconfig.Config.command>
+---@alias EditorStyle "cur_win" | "floating" | "v_split" | "h_split"
