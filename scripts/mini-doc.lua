1bfd5e8a15631bd45856dfa2a96a1882e051cb7c
pete3n <pete3n@protonmail.com>
2025-03-01 13:34:08 -0500

    ci: documentation generation

---
+++ scripts/mini-doc.lua
@@ -0,0 +1,29 @@
+require('mini.doc').setup({
+  hooks = {
+    block_pre = function(block)
+      -- Mark the block if any section has nodoc
+      for _, section in ipairs(block) do
+        if section.info.id == '@nodoc' then
+          block.info.nodoc = true
+          break
+        end
+      end
+    end,
+    block_post = function(block)
+      -- Clear the block if marked for nodoc.
+      if block.info.nodoc then
+        block:clear_lines()
+      end
+    end,
+  },
+})
+
+local input_files = {
+	"../plugin/ninjection.lua",
+	"../lua/ninjection.lua",
+	"../lua/ninjection/config.lua",
+	"../lua/ninjection/types.lua",
+	"../lua/ninjection/health.lua",
+	"../lua/ninjection/treesitter.lua"
+}
+require('mini.doc').generate(input_files, "../doc/ninjection.txt")
