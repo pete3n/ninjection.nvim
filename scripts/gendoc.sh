4f1aaf867c275478edbae939fef748cce87216c1
pete3n <pete3n@protonmail.com>
2025-03-01 14:27:19 -0500

    ci: fix make docs directory if not present

--- scripts/gendoc.sh
+++ scripts/gendoc.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+mkdir -p ../doc
 nvim --headless -c 'luafile ./mini-doc.lua' -c 'qa!' &&
 cat introduction.txt ../doc/ninjection.txt > ../doc/ninjection_final.txt &&
 mv ../doc/ninjection_final.txt ../doc/ninjection.txt
