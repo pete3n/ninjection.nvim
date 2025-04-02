00dbfe5885fa50aadb7ee08c1337e283fa428ba1
pete3n <pete3n@protonmail.com>
2025-04-02 03:46:44 -0400

    style: formatting

--- tests/e2e/helpers/init.lua
+++ tests/e2e/helpers/init.lua
@@ -3,18 +3,18 @@ local rtp = vim.env.NVIM_RTP
 local vimruntime = vim.env.VIMRUNTIME
 
 if packpath and packpath ~= "" then
-  vim.opt.packpath = { packpath }
-  print("Packpath set to:", vim.inspect(vim.opt.packpath:get()))
+	vim.opt.packpath = { packpath }
+	print("Packpath set to:", vim.inspect(vim.opt.packpath:get()))
 end
 
 if rtp and rtp ~= "" then
-  vim.opt.rtp = { rtp }
-  print("Runtime path set to:", vim.inspect(vim.opt.rtp:get()))
+	vim.opt.rtp = { rtp }
+	print("Runtime path set to:", vim.inspect(vim.opt.rtp:get()))
 end
 
 if vimruntime and vimruntime ~= "" then
 	package.path = vimruntime .. "/lua/?.lua;" .. vimruntime .. "/lua/?/init.lua;" .. package.path
-  print("Updated package.path with VIMRUNTIME:", package.path)
+	print("Updated package.path with VIMRUNTIME:", package.path)
 end
 
 local project_root = vim.fn.getcwd()
