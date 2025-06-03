3be0d33ad3e0a0914f98bb8a8f3e1b716f1ef75a
pete3n <pete3n@protonmail.com>
2025-06-02 21:23:07 -0400

    test: revert gv

--- lua/ninjection/init.lua
+++ lua/ninjection/init.lua
@@ -101,7 +101,7 @@ function ninjection.select()
 
 	-- TODO: Implement more precise non-line mode selection
 	ok, result = pcall(function()
-		vim.cmd("normal! v")
+		vim.cmd("normal! gv")
 	end)
 	if not ok then
 		error(tostring(result), 2)
