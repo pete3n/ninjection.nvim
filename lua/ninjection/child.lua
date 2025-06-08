53da86f953e4bb89e308b865880f907f7e7f48d0
pete3n <pete3n@protonmail.com>
2025-06-08 15:48:05 -0400

    fix: circular dependencies, function require

--- lua/ninjection/child.lua
+++ lua/ninjection/child.lua
@@ -6,9 +6,6 @@
 ---@nodoc
 ---@type Ninjection.Config
 local cfg = require("ninjection.config").values
-local create_child_win = require("ninjection.buffer").create_child_win
-local get_indents = require("ninjection.buffer").get_indents
-local is_parent = require("ninjection.parent").is_parent
 
 ---@tag NJChild
 ---@class NJChild
@@ -99,7 +96,7 @@ function NJChild:init_buf(opts)
 	if opts.create_win then
 		---@type integer, string?
 		local c_win, cwin_err
-		c_win, cwin_err = create_child_win(c_bufnr, cfg.editor_style)
+		c_win, cwin_err = require("ninjection.buffer").create_child_win(c_bufnr, cfg.editor_style)
 		if not c_win or cwin_err then
 			---@type string
 			local err = "ninjection.child:init_buf() error: Failed to create child window... " .. tostring(cwin_err)
@@ -178,7 +175,7 @@ function NJChild:init_buf(opts)
 	-- autoformatting, or they will be lost.
 	if cfg.preserve_indents then
 		---@type NJIndents?, string?
-		local p_indents, ind_err = get_indents(0)
+		local p_indents, ind_err = require("ninjection.buffer").get_indents(0)
 		if not p_indents then
 			-- Initialized to 0 if unset
 			p_indents = { t_indent = 0, b_indent = 0, l_indent = 0, tab_indent = 0 }
@@ -282,7 +279,7 @@ function NJChild:get_parent()
 			vim.notify(err, vim.log.levels.ERROR)
 		end
 		return nil, err
-	elseif not get_njp_return or not is_parent(get_njp_return) then
+	elseif not get_njp_return or not require("ninjection.parent").is_parent(get_njp_return) then
 		local err = "ninjection.child.NJChild:get_parent() error: This buffer appears to be an orphan: The child buffer "
 			.. self.c_bufnr
 			.. " has the parent buffer "
