a99a29edbc5d7b0e7e6a5e2d80da8315a65e5acc
pete3n <pete3n@protonmail.com>
2025-06-09 18:16:20 -0400

    test: faster window deletion deferment

--- lua/ninjection/init.lua
+++ lua/ninjection/init.lua
@@ -437,15 +437,6 @@ local function indent_block(bufnr, range, rep_lines)
 	vim.api.nvim_buf_set_lines(bufnr, s_row, e_row + 1, false, formatted_lines)
 end
 
-local function is_buf_visible(bufnr)
-	for _, win in ipairs(vim.api.nvim_list_wins()) do
-		if vim.api.nvim_win_get_buf(win) == bufnr then
-			return true
-		end
-	end
-	return false
-end
-
 ---@tag ninjection.format()
 ---@brief
 --- Formats the injected code block under cursor using a specified format cmd,
@@ -549,17 +540,12 @@ function ninjection.format()
 			indent_block(cur_bufnr, injection.range, rep_lines)
 		end
 
-		for _, win in ipairs(vim.api.nvim_list_wins()) do
-			local buf = vim.api.nvim_win_get_buf(win)
-			vim.notify("Window " .. win .. " has buffer " .. buf)
-		end
-
 		vim.defer_fn(function()
 			-- Defer deletion to allow LSP background operations to finish
 			if nj_child.c_bufnr and vim.api.nvim_buf_is_valid(nj_child.c_bufnr) then
 				vim.api.nvim_buf_delete(nj_child.c_bufnr, { force = true })
 			end
-		end, 150) -- ~150ms delay to avoid race with semantic token requests
+		end, 50)
 	end)
 
 	return true, nil
