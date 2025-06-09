e722bda6ff3d3771d33847c7f8e093bb6f031c50
pete3n <pete3n@protonmail.com>
2025-06-09 13:02:10 -0400

    fix: line offset

--- tests/e2e/format_file_spec.lua
+++ tests/e2e/format_file_spec.lua
@@ -9,13 +9,13 @@ print(vim.inspect(vim.opt.rtp:get()))
 describe("ninjection.format integration test #e2e #edit", function()
 	it("validates injected content after formatting buffer", function()
 		vim.cmd("edit tests/ft/nix/lua.nix")
-		local buf_content = vim.api.nvim_buf_get_lines(0, 10, 13, false)
+		local buf_content = vim.api.nvim_buf_get_lines(0, 9, 13, false)
 		print("Buffer content:", vim.inspect(buf_content))
 		vim.api.nvim_win_set_cursor(0, { 11, 7 })
 		nj.format()
 		vim.wait(500) -- TODO: Replace with appropriate configuration timing
 		local buf = vim.api.nvim_get_current_buf()
-		buf_content = vim.api.nvim_buf_get_lines(buf, 10, 13, false)
+		buf_content = vim.api.nvim_buf_get_lines(buf, 9, 13, false)
 		local expected = {
 			"    ''",
 			"      local lua_content",
