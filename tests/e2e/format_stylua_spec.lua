4a8994fbf19fb16af4c1cf3b5378f0ff3666f31f
pete3n <pete3n@protonmail.com>
2025-06-11 09:22:31 -0400

    test: stylua desc

--- tests/e2e/format_stylua_spec.lua
+++ tests/e2e/format_stylua_spec.lua
@@ -1,20 +1,22 @@
 package.path = vim.fn.getcwd() .. "/tests/e2e/?.lua;" .. package.path
 require("helpers.init")
 
-it("formats Lua with stylua", function()
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
-    "local lua_content",
-    "  local more_lua_content"
-  })
-  vim.bo[buf].filetype = "lua"
+describe("stylua manual format test #e2e #stylua", function()
+	it("formats Lua with stylua", function()
+		local buf = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
+			"local lua_content",
+			"  local more_lua_content",
+		})
+		vim.bo[buf].filetype = "lua"
 
-  require("conform").format({
-    bufnr = buf,
-    async = false,
-    lsp_fallback = false,
-  })
+		require("conform").format({
+			bufnr = buf,
+			async = false,
+			lsp_fallback = false,
+		})
 
-  local out = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-  print("Formatted:", vim.inspect(out))
+		local out = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+		print("Formatted:", vim.inspect(out))
+	end)
 end)
