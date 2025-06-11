7a55c26f1d74eb7dc69bd3d89439cb3d21fd8ae1
pete3n <pete3n@protonmail.com>
2025-06-11 12:10:45 -0400

    test: debug outputs

--- tests/e2e/format_stylua_spec.lua
+++ tests/e2e/format_stylua_spec.lua
@@ -11,24 +11,31 @@ describe("stylua manual format test #e2e #stylua", function()
 		vim.bo[buf].filetype = "lua"
 
 		local conform = require("conform")
-		print("Detected formatters:", vim.inspect(conform.list_formatters_for_buffer(buf)))
 
-		print("stylua path:", vim.fn.exepath("stylua"))
-		print("stylua version:", vim.fn.system("stylua --version"))
-
-		require("conform").setup({
+		-- SETUP MUST BE CALLED FIRST
+		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 			},
+			formatters = {
+				stylua = {
+					prepend_args = { "--indent-width", "2" },
+				},
+			},
 		})
 
-		require("conform").format({
+		-- Confirm stylua is found
+		vim.notify("stylua path: " .. vim.fn.exepath("stylua"))
+		vim.notify("stylua version: " .. vim.fn.system("stylua --version"))
+		vim.notify("Detected formatters: " .. vim.inspect(conform.list_formatters_for_buffer(buf)))
+
+		conform.format({
 			bufnr = buf,
 			async = false,
 			lsp_fallback = false,
 		})
 
 		local out = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-		print("Formatted:", vim.inspect(out))
+		vim.notify("Formatted: " .. vim.inspect(out))
 	end)
 end)
