e342796fbd299c429eee5476bfa7a3415dab02e7
pete3n <pete3n@protonmail.com>
2025-06-11 20:08:07 -0400

    test: 6 lines

--- tests/e2e/format_file_spec.lua
+++ tests/e2e/format_file_spec.lua
@@ -7,13 +7,13 @@ local nj = require("ninjection")
 describe("ninjection.format integration test #e2e #edit", function()
 	it("validates injected content after formatting buffer", function()
 		vim.cmd("edit tests/ft/nix/lua.nix")
-		local buf_content = vim.api.nvim_buf_get_lines(0, 9, 13, false)
+		local buf_content = vim.api.nvim_buf_get_lines(0, 9, 15, false)
 		print("Buffer content:", vim.inspect(buf_content))
 		vim.api.nvim_win_set_cursor(0, { 11, 7 })
 		nj.format()
 		vim.wait(500) -- TODO: Replace with appropriate configuration timing
 		local buf = vim.api.nvim_get_current_buf()
-		buf_content = vim.api.nvim_buf_get_lines(buf, 9, 13, false)
+		buf_content = vim.api.nvim_buf_get_lines(buf, 9, 15, false)
 		local expected = {
 			"		''",
 			"			do",
