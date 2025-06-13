a4a29de006b355c29f3cba73d993c6942b3da3e8
pete3n <pete3n@protonmail.com>
2025-06-13 19:42:32 -0400

    test: safe require

--- tests/e2e/helpers/init.lua
+++ tests/e2e/helpers/init.lua
@@ -2,6 +2,30 @@ local packpath = vim.env.NVIM_PACKPATH
 local rtp = vim.env.NVIM_RTP
 local vimruntime = vim.env.VIMRUNTIME
 
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+local servers = {}
+
+if lspconfig_ok then
+	for name, config in pairs(lspconfig) do
+		if type(config) == "table" and config.cmd then
+			table.insert(servers, {
+				name = name,
+				cmd = config.cmd,
+			})
+		end
+	end
+end
+
+local f = io.open("debug/debug_log.txt", "a")
+if f then
+	f:write("[LSPConfig Dump] Registered servers:\n")
+	for _, server in ipairs(servers) do
+		f:write(string.format("- %s → cmd: %s\n", server.name, vim.inspect(server.cmd)))
+	end
+	f:write("\n")
+	f:close()
+end
+
 if rtp and rtp ~= "" then
 	vim.opt.rtp = { rtp }
 	print("Runtime path set to:", vim.inspect(vim.opt.rtp:get()))
