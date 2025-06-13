836b8805ffd8f2fa509d06c4f390e47ab97c0780
pete3n <pete3n@protonmail.com>
2025-06-13 10:47:24 -0400

    test: increase wait time

--- tests/e2e/format_file_spec.lua
+++ tests/e2e/format_file_spec.lua
@@ -11,7 +11,7 @@ describe("ninjection.format integration test #e2e #edit", function()
 		print("Buffer content:", vim.inspect(buf_content))
 		vim.api.nvim_win_set_cursor(0, { 11, 7 })
 		nj.format()
-		vim.wait(500) -- TODO: Replace with appropriate configuration timing
+		vim.wait(3000) -- TODO: Replace with appropriate configuration timing
 		local buf = vim.api.nvim_get_current_buf()
 		buf_content = vim.api.nvim_buf_get_lines(buf, 9, 15, false)
 		local expected = {
