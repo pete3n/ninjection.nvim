5328db159c27d9d407816067c677cfd432e7013e
pete3n <pete3n@protonmail.com>
2025-06-19 18:21:36 -0400

    test: add lua-nix edit test

---
+++ tests/edit_lua_nix_spec.lua
@@ -0,0 +1,34 @@
+package.path = vim.fn.getcwd() .. "/tests/e2e/?.lua;" .. package.path
+
+local eq = assert.are.same
+local nj = require("ninjection")
+
+describe("ninjection.edit integration test #e2e #nix-lua #edit", function()
+	it("validates injected content in edit buffer", function()
+		vim.cmd("edit /ninjection/tests/ft/nix/lua_edit.nix")
+
+		local p_content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+		if _G.test_debug then
+			print("Buffer content before edit:\n" .. table.concat(p_content, "\n"))
+		end
+
+		vim.api.nvim_win_set_cursor(0, { 5, 7 })
+		nj.edit()
+		local child_buf = vim.api.nvim_get_current_buf()
+		local c_content = vim.api.nvim_buf_get_lines(child_buf, 0, -1, false)
+		local expected = {
+			"let",
+			"  flake = builtins.getFlake (toString ./.);",
+			"in",
+			"if builtins.isAttrs flake.outputs.devShells.x86_64-linux.default then",
+			"  builtins.attrNames flake.outputs.devShells.x86_64-linux.default",
+			"else",
+			'  "LEAF"',
+		}
+		if _G.test_debug then
+			print("Buffer content after edit:\n" .. table.concat(c_content, "\n"))
+		end
+		eq(expected, c_content)
+		vim.cmd("bdelete!")
+	end)
+end)
---
+++ tests/ft/lua/nix_format.lua
@@ -0,0 +1,10 @@
+{ }:
+{
+  injected_content_format = # lua
+         ''
+     do
+ local lua_content
+    local more_lua_content
+        end 
+ '';
+}
