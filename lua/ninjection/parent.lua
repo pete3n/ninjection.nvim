a0907f3aef65585daba9e4b54a3ed225554ab317
pete3n <pete3n@protonmail.com>
2025-06-19 14:04:49 -0400

    refactor: Parent object class, now standalone without children/pre-child

--- lua/ninjection/parent.lua
+++ lua/ninjection/parent.lua
@@ -8,11 +8,14 @@
 local cfg = require("ninjection.config").values
 
 ---@tag NJParent
----@brief Stores associated child bufnrs.
+---@brief Stores parent buffer information and associated child bufnrs.
 ---
 ---@class NJParent
 ---@field type "NJParent"
----@field children integer[]
+---@field p_bufnr integer Parent buffer number
+---@field p_ft string Parent buffer filetype
+---@field p_name string Parent buffer name
+---@field children? integer[] Child bufnrs associated with this parent
 local NJParent = {}
 NJParent.__index = NJParent
 
@@ -32,11 +35,17 @@ function NJParent.is_parent(obj)
 end
 
 ---@param opts {
+---  p_bufnr: integer,
+---  p_ft: string,
+---  p_name: string,
 ---  children?: integer[] }
 ---
 ---@return NJParent
 function NJParent.new(opts)
 	local self = setmetatable({
+		p_bufnr = opts.p_bufnr,
+		p_ft = opts.p_ft,
+		p_name = opts.p_name,
 		children = opts.children,
 	}, NJParent)
 
@@ -46,15 +55,15 @@ function NJParent.new(opts)
 	return self
 end
 
+---@nodoc
+-- Update the ninjection table in the parent buffer.
 -- This overwrites the ninjection table for the buffer if it exists.
--- Ninjection does not support nested parent -> child -> parent buffer relationships,
--- So this shouldn't be an issue. A parent should never become a child and vice-versa.
----@param p_bufnr integer
+-- Ninjection does not support nested parent -> child -> parent buffer relationships.
+-- A parent should never become a child and vice-versa.
 ---@return boolean success, string? err
-function NJParent:set_nj_table(p_bufnr)
-	-- Save the child information to the buffer's ninjection table
-	if not vim.api.nvim_buf_is_valid(p_bufnr) then
-		local err = "ninjection.buffer.init_child() error: Child buffer is invalid."
+function NJParent:update()
+	if not vim.api.nvim_buf_is_valid(self.p_bufnr) then
+		local err = "ninjection.parent:update() error: Bufnr, " .. self.p_bufnr .. " is invalid."
 		if cfg.debug then
 			vim.notify(err, vim.log.levels.ERROR)
 		end
@@ -62,11 +71,11 @@ function NJParent:set_nj_table(p_bufnr)
 	end
 
 	local set_nj_ok = pcall(function()
-		return vim.api.nvim_buf_set_var(p_bufnr, "ninjection", self)
+		return vim.api.nvim_buf_set_var(self.p_bufnr, "ninjection", self)
 	end)
 	if not set_nj_ok then
 		---@type string
-		local err = "ninjection.buffer.init_child() error: Failed to update ninjection table with child object."
+		local err = "ninjection.parent:update() error: Failed to update parent ninjection table."
 		if cfg.debug then
 			vim.notify(err, vim.log.levels.ERROR)
 		end
@@ -76,22 +85,97 @@ function NJParent:set_nj_table(p_bufnr)
 	return true, nil
 end
 
+---@nodoc
+--- Delete a child buffer, remove it from the parent, and update the parent table.
 ---@param c_bufnr integer
 ---@return boolean success, string? err
 function NJParent:del_child(c_bufnr)
 	---@type integer, integer
 	for i, bufnr in ipairs(self.children) do
 		if bufnr == c_bufnr then
+			if c_bufnr and vim.api.nvim_buf_is_valid(c_bufnr) then
+				vim.api.nvim_buf_delete(c_bufnr, { force = true })
+			end
 			table.remove(self.children, i)
 			return true, nil
 		end
 	end
+	self:update()
+
 	---@type string
 	local err = "ninjection.parent:del_child(): bufnr " .. c_bufnr .. " not found in parent."
 	if cfg.debug then
 		vim.notify(err, vim.error.levels.ERROR)
 	end
 	return false, err
+end
+
+---@nodoc
+--- Replace lines in a given range with replacement text.
+---@param rep_lines string[]
+---@param range NJRange
+---@return boolean success, string? err
+function NJParent:replace_range(rep_lines, range)
+	---@type { open: string, close: string }?
+	local delimiters = cfg.format_delimiters[self.p_ft]
+	if not delimiters or type(delimiters) ~= "table" then
+		---@type string
+		local err = "ninjection.parent:replace_range() error: No injected code comment delimiters defined "
+			.. "for the language"
+			.. self.p_ft
+			.. " please check your format_delimiters configuration table."
+		if cfg.debug then
+			vim.notify(err, vim.log.levels.ERROR)
+		end
+		return false, err
+	end
+
+	if not rep_lines or vim.tbl_isempty(rep_lines) then
+		---@type string
+		local err = "ninjection.parent:replace_range() error: No replacement lines found."
+		if cfg.debug then
+			vim.notify(err, vim.log.levels.WARN)
+		end
+		return false, err
+	end
+
+	---@type string
+	local parent_line = vim.api.nvim_buf_get_lines(self.p_bufnr, range.s_row - 1, range.s_row, false)[1] or ""
+	---@type string
+	local parent_indent = parent_line:match("^(%s*)") or ""
+	---@type string
+	local delimiter_indent = parent_indent .. string.rep(" ", cfg.format_indent)
+	---@type string
+	local child_indent = delimiter_indent .. string.rep(" ", cfg.format_indent)
+
+	---@type string[]
+	local delimited_lines = {}
+	table.insert(delimited_lines, delimiter_indent .. delimiters.open)
+	for _, line in ipairs(rep_lines) do
+		table.insert(delimited_lines, child_indent .. line)
+	end
+	table.insert(delimited_lines, delimiter_indent .. delimiters.close)
+
+	---@type boolean, string?
+	local set_lines_ok, set_lines_err =
+		pcall(vim.api.nvim_buf_set_lines, self.p_bufnr, range.s_row, range.e_row + 1, false, delimited_lines)
+	if not set_lines_ok then
+		---@type string
+		local err = "ninjection.parent:replace_range(): Error setting lines in parent bufnr, "
+			.. self.p_bufnr
+			.. " for range, "
+			.. range.s_row
+			.. " to "
+			.. range.e_row
+			.. " ... "
+			.. set_lines_err
+		if cfg.debug then
+			vim.notify(err, vim.log.levels.ERROR)
+		end
+		return false, err
+	end
+
+	return true, nil
 end
 
 return NJParent
